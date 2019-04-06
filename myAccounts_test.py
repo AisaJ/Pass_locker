8152485cd46cc954f39ab46244e0155ece1e37ff
Aisa <aisajemila@gmail.com>
2019-04-06 16:53:56 +0300

    Imported unittest framework and accounts class. Also created a subclass TestAccounts that inherits from unittest.TestCase.

---
+++ myAccounts_test.py
@@ -0,0 +1,11 @@
+import unittest #importing the unittest module
+from accounts import Accounts #importing accounts class
+
+class TestAccounts(unittest.TestCase):
+
+  '''
+  Test class that defines test cases for the accounts class behaviours.
+
+  Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+  '''
