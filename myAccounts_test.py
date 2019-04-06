b3f8a9e14c1317eed4a25afd7f15b0dc514293a8
Aisa <aisajemila@gmail.com>
2019-04-06 22:03:19 +0300

    Added setUp method that allows defination of instructions to be executed before test method. Created a test 'instance_test'

--- myAccounts_test.py
+++ myAccounts_test.py
@@ -9,3 +9,20 @@ class TestAccounts(unittest.TestCase):
   Args:
     unittest.TestCase: TestCase class that helps in creating test cases
   '''
+  def setUp(self):
+    '''
+    Set up method to run before each test cases.
+    '''
+    self.new_accounts = Accounts("LinkedIn","Aisa","jem#jem") #create account object
+
+  def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_accounts.account_name,"LinkedIn")
+    self.assertEqual(self.new_accounts.username, "Aisa")
+    self.assertEqual(self.new_accounts.password, "jem#jem")
+
+if __name__ == '__main__':
+  unittest.main()
