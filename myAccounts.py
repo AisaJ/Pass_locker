f5f6660f57d30768b47306cea9afb2776ce9d25f
Aisa <aisajemila@gmail.com>
2019-04-06 22:42:05 +0300

    Added a new accounts object and passed in three properties.

--- myAccounts.py
+++ myAccounts.py
@@ -1,1 +1,12 @@
+class Accounts:
+  '''
+  Class that generates new instances of accounts
+  '''
+  def __init__(self, account_name,username,password):
+    self.account_name = account_name
+    self.username = username
+    self.password = password
+    
+
+
 
