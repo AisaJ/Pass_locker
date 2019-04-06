48ddd3948cca3930b11683dfd04302e16c558083
Aisa <aisajemila@gmail.com>
2019-04-06 23:15:18 +0300

    Created save_account method, that's called on accounts object and appends to accounts_list.

--- myAccounts.py
+++ myAccounts.py
@@ -3,6 +3,11 @@ class Accounts:
   Class that generates new instances of accounts
   '''
   accounts_list = []
+  def save_account(self):
+    '''
+    save_account method saves accoutn objects into accounts_list
+    '''
+    
   def __init__(self, account_name,username,password):
     self.account_name = account_name
     self.username = username
