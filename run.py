a0a9b9ae8c9b2c85093d92b5aa8d7cb7e77e12f3
Aisa <aisajemila@gmail.com>
2019-04-07 18:09:35 +0300

    Added a main function to call the functions behaviours. Prompt users to create a login account for password locker

--- run.py
+++ run.py
@@ -37,3 +37,27 @@ def display_accounts():
   Function that returns all saved 
   '''
   return Accounts.display_all_accounts()
+
+def main():
+  print("Hi there, welcome to password-Loc. Please create an account")
+
+  loc_username = input("Enter Password-Loc username: ")
+  loc_password = input("Type in your password: ")
+  
+  print(f"Hello {loc_username}. Please Re-Type in your password to continue")
+  print('\n')
+
+  password_confrim = input("Re-type password")
+
+  while password_confrim == loc_password:
+
+    print("Awesome! Select from the short-code options to begin: ca - create new account details, ac - Add new account details, fa - find account details, da - display accounts saved, da - delete an account record")
+
+    option = input().lower()
+
+    if option == 'ca':
+      print("Account name")
+
+if __name__ == '__main__':
+  main()
+
