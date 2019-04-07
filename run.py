7c7967b6df927b8df78dd1c8963f4b0493246ea8
Aisa <aisajemila@gmail.com>
2019-04-07 15:41:33 +0300

    Added functions to save,delete,display and check for an existing account saved in the run.py file - calls functions in the class.

--- run.py
+++ run.py
@@ -8,5 +8,33 @@ def add_account(account,usr_name,a_password):
   new_myaccounts = Accounts(account,usr_name,a_password)
   return new_myaccounts
 
-def save_accounts()
+def save_accounts(myaccounts):
+  '''
+  Function to save contact
+  '''
+  myaccounts.save_account()
+
+def delete_accounts(myaccounts):
+  '''
+  Function to delete an account
+  '''
+  myaccounts.delete_account()
+
+def find_account(name):
+  '''
+  Function that finds an account by account-name and returns the account
+  '''
+  return Accounts.find_by_name(name)
+
+def view_existing_accounts(name):
+  '''
+  Function that check if an account exists with account name and return a boolean
+  '''
+  return Accounts.account_exists(name)
+
+def display_accounts():
+  '''
+  Function that returns all saved 
+  '''
+  return Accounts.display_all_accounts()
 
